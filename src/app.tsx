30017c963b02281309efbc833b35f51e7b55274a
guoliang <guoliang@guoliangdeMacBook-Pro.local>
2022-05-30 00:14:51 +0800

    feat: configuring webpack to support less/antd/module-css

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Layout, Button } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+export default function App() {
+  return (
+    <div>
+      <Button type="primary">sss</Button>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWepackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const styleRegex = /\.(less|css)$/;
+const moduleRegex = /\.module\.(less|css)$/;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[hash:5].js",
+  },
+  devtool: "source-map",
+  devServer: {
+    host: "127.0.0.1",
+    port: 8080,
+    open: true,
+    historyApiFallback: {
+      index: "./index.html",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "~": path.resolve(__dirname, "node_modules"),
+    },
+    extensions: [".tsx", "jsx", ".ts", ".js", ".json"], // 引入模块没有给扩展名，
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              presets: [
+                ["@babel/preset-env"],
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                  },
+                ],
+                [
+                  "@babel/typescript",
+                  {
+                    isTSX: true,
+                    allExtensions: true,
+                  },
+                ],
+              ],
+              plugins: [
+                [
+                  "import",
+                  {
+                    libraryName: "antd",
+                    libraryDirectory: "es",
+                    style: "css",
+                  },
+                ],
+                ["@babel/plugin-transform-runtime"],
+                ["@babel/plugin-proposal-decorators", { legacy: true }],
+                ["@babel/plugin-proposal-class-properties", { loose: true }],
+              ],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+
+      {
+        test: styleRegex,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env"]],
+              },
+            },
+          },
+          "less-loader",
+        ],
+        // exclude: /node_module/,
+      },
+      {
+        test: moduleRegex,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: ture,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env"]],
+              },
+            },
+          },
+          "less-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWepackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
